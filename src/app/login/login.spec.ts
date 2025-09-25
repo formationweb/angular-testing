bc2662ba6fd0e731f8c7ff92cfdef0fea14f8004
RSamaium <sam@newbot.io>
2025-09-25 15:22:48 +0200

    login spec

--- src/app/login/login.spec.ts
+++ src/app/login/login.spec.ts
@@ -5,6 +5,7 @@ import { Login } from './login';
 describe('Login', () => {
   let component: Login;
   let fixture: ComponentFixture<Login>;
+  let el: HTMLElement
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,51 @@ describe('Login', () => {
 
     fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
+    el = fixture.nativeElement
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('La validité/invalidité du champ email selon la saisie.', () => {
+    const email =  component.propEmail
+    email.setValue('')
+    expect(email.valid).toBeFalse()
+    expect(email.hasError('required')).toBeTrue()
+  })
+
+  it('Le comportement du champ password.', () => {
+    const password = component.propPass
+    expect(password.valid).toBeTrue()
+  })
+
+  describe('FormGroup', () => {
+    it('La validité globale du formulaire. (invalid)', () => {
+      component.form.setValue({
+        email: '',
+        password: ''
+      })
+      expect(component.form.valid).toBeFalse()
+    })
+
+    it('La validité globale du formulaire. (valid)', () => {
+      component.form.setValue({
+        email: 'aaaa@aaaa.net',
+        password: ''
+      })
+      expect(component.form.valid).toBeTrue()
+    })
+
+    it('Que login() modifie bien l’état submitted.', () => {
+      expect(component.submitted()).toBeFalse()
+      component.login()
+      expect(component.submitted()).toBeTrue()
+    })
+
+    it('Le bouton appelle login()', () => {
+      const spy = spyOn(component, 'login')
+      const formEl = el.querySelector('form')
+      formEl?.dispatchEvent(new Event('submit'))
+      fixture.detectChanges()
+      expect(spy).toHaveBeenCalled()
+    })
+  })
 });
